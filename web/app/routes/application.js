1dd125ccc9cad22ea4991ee98dd1f76eb916f3f6
Joseph Pedicini <joey.pedicini@gmail.com>
2019-01-12 21:35:29 -0500

    fix some things

--- web/app/routes/application.js
+++ web/app/routes/application.js
@@ -1,13 +1,16 @@
+import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 import Ember from 'ember';
-export default Ember.Route.extend({
-  session: Ember.inject.service(),
+
+export default Route.extend({
+  session: service(),
   beforeModel: function () {
     return this.get('session').fetch().catch(function () { });
   },
   actions: {
     signIn: function (provider) {
       this.get('session').open('firebase', { provider: provider }).then(function (data) {
-        console.log(data.currentUser);
+        Ember.Logger.debug(data.currentUser);
       });
     },
     signOut: function () {
